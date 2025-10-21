87e46d8cdfc5e788fc9ddec49087024a659a560d
Angus-Eastell <sd23327@bristol.ac.uk>
2025-10-21 11:16:13 +0100

    latlon

--- Merge_csv.py
+++ Merge_csv.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-st = pd.read_csv('stations.csv', low_memory=False)
-st_hour = pd.read_csv('station_hour.csv', low_memory=False)
+
+
+st = pd.read_csv("C:\\Users\\angus\\MDM3\\Air_Quality_Prediction\\stations_with_coords.csv", low_memory=False)
+
+st_hour = pd.read_csv('C:\\Users\\angus\\MDM3\\Air_Quality_Prediction\\station_day.csv', low_memory=False)
 
 for df in (st, st_hour):
     if 'StationId' in df.columns:
@@ -9,12 +12,12 @@ for df in (st, st_hour):
     if 'City' in df.columns:
         df['City'] = df['City'].astype(str).str.strip()
 
-st_hour['datetime'] = pd.to_datetime(st_hour['Datetime'], errors='coerce')
-st_hour = st_hour.drop(columns=['Datetime'])
+st_hour['datetime'] = pd.to_datetime(st_hour['Date'], errors='coerce')
+st_hour = st_hour.drop(columns=['Date'])
 st_hour = st_hour.drop(columns=[c for c in ['Benzene','Toluene','Xylene'] if c in st_hour.columns], errors='ignore')
 
 panel = st_hour.merge(
-    st[['StationId','StationName','City','State','Status']],
+    st[['StationId','StationName','City','State','Status', 'lat', 'lon']],
     on='StationId', how='left', validate='many_to_one'
 )
 
@@ -26,10 +29,10 @@ for c in polls:
 if {'StationId','datetime'}.issubset(panel.columns):
     panel = panel.drop_duplicates(subset=['StationId','datetime'])
 
-front = ['datetime','StationId','StationName','City','State','Status']
+front = ['datetime','StationId','StationName','City','State','lat', 'lon', 'Status',]
 meas  = ['PM2.5','PM10','NO','NO2','NOx','NH3','CO','SO2','O3','AQI','AQI_Bucket']
 ordered = [c for c in front if c in panel.columns] + [c for c in meas if c in panel.columns] + [c for c in panel.columns if c not in set(front+meas)]
 panel = panel[ordered]
 
-panel.to_parquet('India_complete.parquet', index=False, compression='zstd')
-panel.to_csv('India_complete.csv', index=False)
+#panel.to_parquet('India_complete_day.parquet', index=False, compression='zstd')
+panel.to_csv('India_complete_day.csv', index=False)
